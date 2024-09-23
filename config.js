6d3147631dda4563f52bd04d45b7ea0ea1ff43db
Matele9 <saixjunior3@gmail.com>
2024-09-24 00:57:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "levanter_2317dac9a0fd074ad4b27c16e6ec0c95df",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
 MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
